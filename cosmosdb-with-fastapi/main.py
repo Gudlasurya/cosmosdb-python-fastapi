4cdb8e0f0e2de10c4a5f9ba3e0c68fa42fae3230
Ubuntu <azureuser@azuretest.jinyal3qsxvefcbjrn4occnqsb.bx.internal.cloudapp.net>
2022-12-26 11:45:13 +0000

    updated main.py

--- cosmosdb-with-fastapi/main.py
+++ cosmosdb-with-fastapi/main.py
@@ -18,9 +18,9 @@ app.include_router(todo_router, tags=["todos"], prefix="/todos")
 @app.on_event("startup")
 async def startup_db_client():
    # app.cosmos_client = CosmosClient(config["URI"], credential = config["KEY"])
-   app.cosmos_client = CosmosClient(URI, credential = KEY)
-    await get_or_create_db(DATABASE_NAME)
-    await get_or_create_container(CONTAINER_NAME)
+     app.cosmos_client = CosmosClient(URI, credential = KEY)
+     await get_or_create_db(DATABASE_NAME)
+     await get_or_create_container(CONTAINER_NAME)
 
 
 async def get_or_create_db(db_name):
